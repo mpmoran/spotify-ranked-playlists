f73eab74db2a05576b236fdb639976a5814107e4
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2021-03-06 11:49:04 -0600

    move functions to separate file

--- main.py
+++ main.py
@@ -8,104 +8,8 @@ import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-
-# functions
-# ----------------------------------------------------------------------------
-def chunk(iterable, n):
-    """
-    Yield successive n-sized chunks from iterable.
-    """
-    for i in range(0, len(iterable), n):
-        yield iterable[i:i + n]
-
-
-def get_ranked_tracks(df: pd.DataFrame,
-                      metric: str,
-                      num: int,
-                      ascending: bool = False) -> List[dict]:
-    """
-    Return num tracks ranked by metric.
-    """
-    df_ranked = df.sort_values(by=metric,
-                               ascending=ascending,
-                               ignore_index=True)
-    tracks: List[str] = df_ranked['uri'].iloc[:num].tolist()
-
-    return tracks
-
-
-def spotify_create_and_fill_playlist(client: spotipy.client.Spotify,
-                                     name: str,
-                                     track_uris: List[dict],
-                                     description: str,
-                                     user_id: str) -> Union[List[dict], None]:
-    """
-    Create a playlist and fill it with tracks. If a playlist of the same name
-    exists, this function does nothing. Returns information about playlist
-    that was created or None.
-    """
-    # get all playlist names
-    playlists: List[dict] = spotify_get_results(client,
-                                                'user_playlists',
-                                                user=user_id)
-    playlist_names = [pl['name'] for pl in playlists]
-
-    if name in playlist_names:
-        return None
-    else:
-        create_response = client.user_playlist_create(
-            user=user_id,
-            name=name,
-            public=False,
-            collaborative=False,
-            description=description
-        )
-        playlist_id: str = create_response['id']
-        client.playlist_add_items(playlist_id=playlist_id,
-                                  items=track_uris,
-                                  position=None)
-
-        details = {}
-        details['id'] = create_response['id']
-        details['uri'] = create_response['uri']
-
-        return details
-
-
-def spotify_get_results(client: spotipy.client.Spotify,
-                        operation: str,
-                        limit: int = 50,
-                        offset: int = 0,
-                        **kwargs) -> List[dict]:
-    """
-    Return results of calling operation on Spotify client
-    """
-    # get client operation method
-    method = getattr(client, operation)
-
-    # keep making API calls and collecting results
-    results: List[dict] = []
-    while True:
-        response: dict = method(**kwargs,
-                                limit=limit,
-                                offset=offset)
-        items: list = response['items']
-        results.extend(items)
-        whats_left = response['next']
-        if whats_left is None:
-            break
-        offset += limit
-
-    # ensure the number of expected items matches the number of actuals
-    num_expected: int = response['total']
-    num_actual: int = len(results)
-    if num_expected != num_actual:
-        raise Exception(f'Total tracks according to Spotify ({num_expected}) '
-                        f'does not equal total tracks extracted from API '
-                        f'calls ({num_actual}).')
-
-    return results
-# ----------------------------------------------------------------------------
+# local libraries
+import spotify_helpers as sh
 
 
 # load env vars from .env file
@@ -137,8 +41,8 @@ user_uri: str = user_details['uri']
 
 # get all saved tracks by user
 # ----------------------------------------------------------------------------
-saved_tracks: List[dict] = spotify_get_results(sp,
-                                               'current_user_saved_tracks')
+saved_tracks: List[dict] = sh.spotify_get_results(sp,
+                                                  'current_user_saved_tracks')
 num_saved_tracks: int = len(saved_tracks)
 # ----------------------------------------------------------------------------
 
@@ -146,7 +50,7 @@ num_saved_tracks: int = len(saved_tracks)
 # ----------------------------------------------------------------------------
 # audio_features is in the same order as saved_tracks
 audio_features: List[dict] = []
-for piece in chunk(saved_tracks, 100):  # piece is List[dict]
+for piece in sh.chunk(saved_tracks, 100):  # piece is List[dict]
     track_uris: List[str] = [track['track']['uri'] for track in piece]
     results: List[dict] = sp.audio_features(track_uris)
     audio_features.extend(results)
@@ -244,11 +148,11 @@ for pl in playlist_config:
     num_tracks = pl['num_tracks']
     description = pl['description']
 
-    playlist_tracks: List[dict] = get_ranked_tracks(df,
-                                                    metric=metric,
-                                                    num=num_tracks)
+    playlist_tracks: List[dict] = sh.get_ranked_tracks(df,
+                                                       metric=metric,
+                                                       num=num_tracks)
     playlist_details: Union[List[dict], None] = \
-        spotify_create_and_fill_playlist(
+        sh.spotify_create_and_fill_playlist(
             sp,
             name=name,
             track_uris=playlist_tracks,
---
+++ spotify_helpers.py
@@ -0,0 +1,102 @@
+# standard libraries
+from typing import List, Union
+
+# third-party libraries
+import pandas as pd
+import spotipy
+
+
+def chunk(iterable, n):
+    """
+    Yield successive n-sized chunks from iterable.
+    """
+    for i in range(0, len(iterable), n):
+        yield iterable[i:i + n]
+
+
+def get_ranked_tracks(df: pd.DataFrame,
+                      metric: str,
+                      num: int,
+                      ascending: bool = False) -> List[dict]:
+    """
+    Return num tracks ranked by metric.
+    """
+    df_ranked = df.sort_values(by=metric,
+                               ascending=ascending,
+                               ignore_index=True)
+    tracks: List[str] = df_ranked['uri'].iloc[:num].tolist()
+
+    return tracks
+
+
+def spotify_create_and_fill_playlist(client: spotipy.client.Spotify,
+                                     name: str,
+                                     track_uris: List[dict],
+                                     description: str,
+                                     user_id: str) -> Union[List[dict], None]:
+    """
+    Create a playlist and fill it with tracks. If a playlist of the same name
+    exists, this function does nothing. Returns information about playlist
+    that was created or None.
+    """
+    # get all playlist names
+    playlists: List[dict] = spotify_get_results(client,
+                                                'user_playlists',
+                                                user=user_id)
+    playlist_names = [pl['name'] for pl in playlists]
+
+    if name in playlist_names:
+        return None
+    else:
+        create_response = client.user_playlist_create(
+            user=user_id,
+            name=name,
+            public=False,
+            collaborative=False,
+            description=description
+        )
+        playlist_id: str = create_response['id']
+        client.playlist_add_items(playlist_id=playlist_id,
+                                  items=track_uris,
+                                  position=None)
+
+        details = {}
+        details['id'] = create_response['id']
+        details['uri'] = create_response['uri']
+
+        return details
+
+
+def spotify_get_results(client: spotipy.client.Spotify,
+                        operation: str,
+                        limit: int = 50,
+                        offset: int = 0,
+                        **kwargs) -> List[dict]:
+    """
+    Return results of calling operation on Spotify client
+    """
+    # get client operation method
+    method = getattr(client, operation)
+
+    # keep making API calls and collecting results
+    results: List[dict] = []
+    while True:
+        response: dict = method(**kwargs,
+                                limit=limit,
+                                offset=offset)
+        items: list = response['items']
+        results.extend(items)
+        whats_left = response['next']
+        if whats_left is None:
+            break
+        offset += limit
+
+    # ensure the number of expected items matches the number of actuals
+    num_expected: int = response['total']
+    num_actual: int = len(results)
+    if num_expected != num_actual:
+        raise Exception(f'Total tracks according to Spotify ({num_expected}) '
+                        f'does not equal total tracks extracted from API '
+                        f'calls ({num_actual}).')
+
+    return results
